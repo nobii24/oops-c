dc3a2aed4181d2e3e31b0f00cbed6b0213245873
Sameer Patel <sp240603@gmail.com>
2025-02-04 01:09:19 +0530

    Add files via upload

---
+++ demo1.c
@@ -0,0 +1,10 @@
+// C Program to print Hello, World! in the output
+#include<stdio.h>
+
+int main()
+{
+    // printf function is used to print the string argument in the output window
+    printf("Hello, World!\n");
+    
+    return 0;
+}
